cc5fdc8c423e6b48f8cfaa46e010edc2f8cd7782
aleph_v <20227705+aleph-v@users.noreply.github.com>
2025-12-22 16:02:18 -0500

    Add seqeuncer subsystem

--- src/Entrypoint.sol
+++ src/Entrypoint.sol
@@ -10,14 +10,31 @@ import "./Withdraw.sol";
 // This is the main entrypoint for seqeuncing and handles the percentage payouts.
 // Through the inheritance system this pulls in all of the logic needed.
 
+// TODO - Look into ways to minimize the cost of the tracking system.
+
 contract Entrypoint is Withdraw, DepositChallenge, TransactionChallenge, NullifierChallenge, TreeUpdateChallange {
-    mapping(uint256 => uint256) public totalTx;
-    mapping(uint256 => mapping(address => uint256)) public sequencerTx;
+    // Here we track the percent rewards per epoc for the the 
+    mapping(uint256 => uint256) public totalBlobUse;
+    mapping(uint256 => mapping(address => uint256)) public sequencerBlobUse;
+
+    uint256 priorityBonus = 2e3;
+    uint256 constant BASE = 1e3;
+    uint256 constant FIXED_BASE  = 1e18;
 
     // The function which allows sequencers to post
-    function post() external {
-        // Check using the sequencer registry functions we can post
-        // then push up the data to spine
+    function post(
+        BlockData memory data, 
+        uint256[] memory blobIndicies
+    ) external {
+        require(isAllowed(msg.sender));
+        addBlock(data, blobIndicies);
+        (uint256 epoc, bool currentlyPriorty) = currentEpoc();
+
+        // Tracking this basis of blob data usage gives a fair tradeoff on cost
+        uint256 rawBlobUse = data.numTransactions*15 + data.numDeposits*4;
+        uint256 adjustedTx = currentlyPriorty? rawBlobUse*priorityBonus/BASE: rawBlobUse;
+        totalTx[epoc] += adjustedTx;
+        sequencerBlobUse[epoc][msg.sender] += adjustedTx;
     }
 
     function allocateRewards(uint256 epocNumber, address sequencer) external {
--- src/SequencerRegistry.sol
+++ src/SequencerRegistry.sol
@@ -1,50 +1,63 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Ownable} from "solady/auth/Ownable.sol";
+
 // The module which handles the registration of the sequencers
 
-contract SequencerRegistry {
+// TODO - Yield System integration
+
+contract SequencerRegistry is Ownable {
     uint256 constant EPOC_LENGTH = 10;
-    uint256 immutable start;
+    uint256 constant CHALLENGE_WINDOW = 10;
+    uint256 immutable START = block.timestamp;
+    uint256 constant STAKE_DIVISOR = 10^15;
+    uint256 constant MAX_STAKE = 200 ether / STAKE_DIVISOR;
 
-    uint256 requiredStake;
+    uint256 requiredStake = 20 ether / STAKE_DIVISOR;
 
-    struct sequencerStatus {
+    // TODO - Optimize packing
+    struct SequencerStatus {
         bool isActive;
         bool isPriority;
-        bool challangeClaimed;
-        address challenger;
+        uint8 priorityIndex;
         uint64 blocknumberChallanged;
         uint64 timestampChallanged;
+        uint64 stakeAmount;
+        address payable challenger;
     }
-    mapping(address => sequencerStatus) sequencers;
+    mapping(address => SequencerStatus) sequencers;
+    mapping(address => uint256) exits;
     address[] firstLookSequncers;
 
     // Checks if (1) the user is a registered sequencer (2) if the time is within the reserved seqeuncing period
     // then that this is firstLookSequncer
     function isAllowed(address sequencer) public view returns (bool) {
-        return true;
+        (uint256 current, bool isClosed) = currentEpoc();
+        if (isClosed) {
+            return (sequencer == firstLookSequncers[current % firstLookSequncers.length]);
+        }
+        return sequencers[sequencer].isActive && (sequencers[sequencer].stakeAmount >= requiredStake);
     }
 
     // Computes the epoc and returns if we are in the first half of an epoc
     function currentEpoc() public view returns (uint256, bool) {
-        uint256 epoc = (block.timestamp - start) / EPOC_LENGTH;
+        uint256 epoc = (block.timestamp - START) / EPOC_LENGTH;
         // The rounding error here tells us how much of the epoc has passed.
-        uint256 elapsed = block.timestamp - (epoc * EPOC_LENGTH + start);
+        uint256 elapsed = block.timestamp - (epoc * EPOC_LENGTH + START);
         return (epoc, elapsed < EPOC_LENGTH / 2);
     }
 
     // Take the money from the sequncer then
-    function register() external payable {
-        require(msg.value > requiredStake);
+    function fund() external payable {
         require(sequencers[msg.sender].challenger == address(0));
         // TODO Need to trigger deposit into the yield system
         sequencers[msg.sender].isActive = true;
+        sequencers[msg.sender] += (msg.value/STAKE_DIVISOR)
     }
 
     function slash(address sequencer, uint256 blockNumber) internal {
         sequencers[sequencer].isActive = false;
-        sequencers[sequencer].isPriority = false;
         sequencers[sequencer].timestampChallanged = (uint64)(block.timestamp);
 
         if (
@@ -57,9 +70,67 @@ contract SequencerRegistry {
             // TODO Probally there is a better way?
             sequencers[sequencer].blocknumberChallanged == blockNumber;
         }
+
+        if (sequencers[sequencer].isPriority) {
+            _remove(sequencers[sequencer].priorityIndex);
+        }
     }
 
-    function claimChallanged(address sequencer) external {
-        // TODO the user who has done the deepest depth challange gets half the reward after time elapsed has passed boundry.
+    function claimChallangeReward(address who) external {
+        SequencerStatus memory status = sequencers[who];
+        uint256 challangeTime = uint256(status.timestampChallanged);
+        require(challangeTime != 0 && block.timestamp - challangeTime >= CHALLENGE_WINDOW, "Not ready");
+        delete sequencers[who];
+        (bool success,) = status.challenger.call{value: status.stakeAmount * STAKE_DIVISOR / 2}("");
+        require(success, "Payout failed");
+        // TODO burn the other half into yield using the yield system
+    }
+
+    function registerExit() external {
+        require(sequencers[msg.sender].isActive);
+        sequencers[msg.sender].isActive = false;
+        if (sequencers[msg.sender].isPriority) {
+            _remove(sequencers[msg.sender].priorityIndex);
+        }
+        exits = block.timestamp;
+    }
+
+    function exit(address who) external {
+        require(exits[who] != 0 && block.timestamp - exits[who] >= CHALLENGE_WINDOW, "Exit pending");
+        SequencerStatus memory status = sequencers[who];
+        require(status.challenger == address(0));
+        // Now we can remove and refund them
+        delete sequencers[who];
+        (bool success,) = payable(who).call{value: status.stakeAmount * STAKE_DIVISOR}("");
+        // TODO burn the other half into yield using the yield system
+        require(success, "Payout failed");
+    }
+
+    function addFirstLook(address who) external onlyOwner {
+        require(sequencers[who].isActive);
+        firstLookSequncers.push(who);
+        sequencers[who].isPriority = true;
+    }
+
+    function removeFirstLook(uint256 which) public onlyOwner {
+        _remove(which);
+    }
+
+    // NOTE - Invalid uses of this will lock the seqeuncing
+    function updateStake(uint256 amount) public onlyOwner {
+        require(amount < MAX_STAKE);
+        requiredStake = amount;
+    }
+
+    function _remove(uint256 which) internal {
+        // Remove their status
+        address who = firstLookSequncers[which];
+        sequencers[who].isPriority = false;
+        // Delete from array
+        uint256 lenAfter = firstLookSequncers.length - 1;
+        firstLookSequncers[which] = firstLookSequncers[lenAfter];
+        assembly {
+            sstore(firstLookSequncers.slot, lenAfter)
+        }
     }
 }
