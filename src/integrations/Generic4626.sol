14e4f32a6d3e1e84c5b43e7ec38a069d8f2036fa
aleph_v <20227705+aleph-v@users.noreply.github.com>
2026-01-07 11:37:23 +0100

    fmt afer the foundry update

--- src/integrations/Generic4626.sol
+++ src/integrations/Generic4626.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.13;
 // We may have many special purpose versions of these based on the yield integrations.
 
 contract Generic4626 {
-// Implements a 4626 inteface for our yield source on a per asset basis.
-}
+    // Implements a 4626 interface for our yield source on a per asset basis.
+
+    }
